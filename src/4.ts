0cec117b0301874a9226de03956a855ed261de85
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-11-20 11:30:02 +0200

    finish HW3

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,55 @@
-const key = new Key();
+class Key {
+	private signature: number;
 
+	constructor() {
+		this.signature = Math.random();
+	}
+
+	getSignature(): number {
+		return this.signature;
+	}
+}
+
+class Person {
+	constructor(private key: Key) {}
+
+	getKey(): Key {
+		return this.key;
+	}
+}
+
+abstract class House {
+	protected door: boolean;
+	protected tenants: Person[];
+
+	constructor(protected key: Key) {
+		this.door = false;
+		this.tenants = [];
+	}
+
+	abstract openDoor(key: Key): void;
+
+	comeIn(person: Person): void {
+		if (this.door) {
+			this.tenants.push(person);
+		}
+	}
+}
+
+class MyHouse extends House {
+	openDoor(key: Key): void {
+		if (this.key.getSignature() === key.getSignature()) {
+			this.door = true;
+		}
+	}
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
